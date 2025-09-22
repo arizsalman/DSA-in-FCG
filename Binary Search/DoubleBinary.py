5ff7071911c2f85cf74a06d3bc90b090266ba2ea
arizsalman <muhammadarizsalman@gmail.com>
2025-09-22 19:14:24 +0500

    Done ! 👍

--- Binary Search/DoubleBinary.py
+++ Binary Search/DoubleBinary.py
@@ -3,53 +3,75 @@
 # nums = [5, 7, 7, 8, 8, 10], target = 8, [3, 4]
 
 
-def find(nums,  target):
+# def find(nums,  target):
 
-    def first(nums, target):
-        left, right = 0, len(nums)-1
-        find = -1
-        while left <= right:
-            mid = (left + right)//2
-            if nums[mid] == target:
-                find = mid
-                right = mid-1
-            elif nums[mid] < target:
-                left = mid+1
-            else:
-                right = mid-1
-        return find
+#     def first(nums, target):
+#         left, right = 0, len(nums)-1
+#         find = -1
+#         while left <= right:
+#             mid = (left + right)//2
+#             if nums[mid] == target:
+#                 find = mid
+#                 right = mid-1
+#             elif nums[mid] < target:
+#                 left = mid+1
+#             else:
+#                 right = mid-1
+#         return find
 
-    def last(nums, target):
-        left, right = 0, len(nums)-1
-        find = -1
-        while left <= right:
-            mid = (left + right)//2
-            if nums[mid] == target:
-                find = mid
-                left = mid+1
-            elif nums[mid] < target:
-                left = mid+1
-            else:
-                right = mid-1
-        return find
-    return first(nums, target), last(nums, target)
-
-
-print(find(nums=[5, 7, 7, 8, 8, 10], target=8))
-print(find(nums=[5, 7, 7, 8, 8, 10], target=6))
+#     def last(nums, target):
+#         left, right = 0, len(nums)-1
+#         find = -1
+#         while left <= right:
+#             mid = (left + right)//2
+#             if nums[mid] == target:
+#                 find = mid
+#                 left = mid+1
+#             elif nums[mid] < target:
+#                 left = mid+1
+#             else:
+#                 right = mid-1
+#         return find
+#     return first(nums, target), last(nums, target)
 
 
-# arr = [2,3,4,7,11], k = 5
-def func(arr, k):
-    left, right = 0, len(arr)-1
+# print(find(nums=[5, 7, 7, 8, 8, 10], target=8))
+# print(find(nums=[5, 7, 7, 8, 8, 10], target=6))
+
+
+# # arr = [2,3,4,7,11], k = 5
+# def func(arr, k):
+#     left, right = 0, len(arr)-1
+#     while left <= right:
+#         mid = (left+right)
+#         missing = arr[mid]-(mid+1)
+#         if missing < k:
+#             left = mid+1
+#         else:
+#             right = mid-1
+#     return left + k
+
+
+# print(func(arr=[2, 3, 4, 7, 11], k=5))
+
+# [2, 4, 7, 10, 15], x = 6 7
+
+
+def funcs(arri, x):
+    left, right = 0, len(arri)-1
+    find = -1
     while left <= right:
-        mid = (left+right)
-        missing = arr[mid]-(mid+1)
-        if missing < k:
-            left = mid+1
-        else:
+        mid = (left+right)//2  # 2
+        print(f'mid {mid}')
+        if arri[mid] >= x:
+            find = arri[mid]
             right = mid-1
-    return left + k
+            print(f"right {right}")
+        else:
+            left = mid + 1
+            print(f'left {left}')
+    return find
 
 
-print(func(arr=[2, 3, 4, 7, 11], k=5))
+print(funcs(arri=[2, 4, 7, 10, 15], x=6))
+# print(funcs(arri=[2, 4, 7, 10, 15], x=16))
