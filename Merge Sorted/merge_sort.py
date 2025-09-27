1a0c135be91d9159170bd7dd07f3691c58fa9576
arizsalman <muhammadarizsalman@gmail.com>
2025-09-27 15:14:55 +0500

    leetcode no 148 👍

--- Merge Sorted/merge_sort.py
+++ Merge Sorted/merge_sort.py
@@ -27,4 +27,69 @@ def func(nums):
 """ ye hum ne skeleton  bana he ke ye kam kese karta he """
 
 
-print(func([2, 6, 3, 5, 1, 2, 4, 9]))
+# print(func([2, 6, 3, 5, 1, 2, 4, 9]))
+
+
+def merge(left, right):
+    j = i = 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+def fun(nums):
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums)//2
+    left = nums[:mid]
+    right = nums[mid:]
+    left_sort, right_sort = fun(left), fun(right)
+    return merge(left_sort, right_sort)
+
+
+print(fun([2, 6, 3, 5, 1, 2, 4, 9]))
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+def merge_two(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    num1 = (left[i:])
+    num2 = (right[j:])
+    return result+num1+num2
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge_two(left, right)
+
+
+def main(arr):
+    return merge_sort(arr)
+
+
+print(main([8, 3, 3, 5, 6, 1, 8, 2, 9]))
