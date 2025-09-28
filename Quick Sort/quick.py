5afb3b0bad111695f26419466bffcb3c56d8b1ae
arizsalman <muhammadarizsalman@gmail.com>
2025-09-28 21:07:21 +0500

    Done 👍

--- Quick Sort/quick.py
+++ Quick Sort/quick.py
@@ -40,3 +40,36 @@ def partition(nums, start=0, end=None):
 
 
 print(func([4, 2, 6, 3, 4, 6, 2, 1]))
+
+
+def func(nums, start=0, end=None):
+    if end is None:
+        # ye copy nahe hu ke ab or agar modify karho tu list me array nahe de
+        nums = list(nums)
+        end = len(nums)-1
+    if start < end:
+        pivot = partition(nums, start, end)
+        func(nums, start, pivot-1)  # yaha array ke copy nahe hu rahe
+        func(nums, pivot+1, end)
+    return nums
+
+
+def partition(nums, start=0, end=None):
+    if end is None:
+        end = len(nums)-1
+    left, right = start, end-1
+    while right > left:
+        if nums[left] <= nums[end]:
+            left += 1
+        elif nums[right] > nums[end]:
+            right -= 1
+        else:
+            nums[left], nums[right] = nums[right], nums[left]
+    if nums[left] > nums[end]:
+        nums[left], nums[end] = nums[end], nums[left]
+        return left
+    else:
+        return end
+
+
+print(func([4, 2, 6, 3, 4, 6, 2, 1]))
