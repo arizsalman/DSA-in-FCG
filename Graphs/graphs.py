b63b6337d970055799914472e4c4d9359e6dd40e
arizsalman <muhammadarizsalman@gmail.com>
2025-10-11 15:31:40 +0500

    Done
    Breath First Search : start 👍

--- Graphs/graphs.py
+++ Graphs/graphs.py
@@ -55,45 +55,86 @@ Ek 2D array jisme 1 dikhata hai ke connection hai aur 0 dikhata hai ke nahi."""
 # print(n, m)
 
 
-# class Graph:
-#     def __init__(self, n_vertices, edges):
-#         self.data = [[]for _ in range(n_vertices)]
-#         for v1, v2 in edges:
-#             self.data[v1].append(v2)
-#             self.data[v2].append(v1)
-
-#     def __repr__(self):
-#         result = ""
-#         for i, j in enumerate(self. data):
-#             result += f" {i}:{j}\n"  # is me pura age ga += ke wajah se
-#             # result = f" {i}:{j}\n" # is me sirf ye ae ga  4:[1, 3, 0]  pur nahe .
-#             """to har iteration me result overwrite ho jaata —
-#                sirf last line bachi rehti.
-#                Isliye += use karte hain, taki har line append (add) hoti jaaye."""
-#         return result
-
-
-# graphs = Graph(5, [(0, 1), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4), (4, 0)])
-
-# print(graphs)
-
-
-class GraphMatrix:
+class Graph:
     def __init__(self, n_vertices, edges):
-        # Intailize a matrix of all zeros
-        self.data = [[0 for _ in range(n_vertices)]for _ in range(n_vertices)]
-        """Loop chal ke 4 rows bana chuka hai ✅
-           Lekin print karte waqt Python ne sab ek hi line me dikha diya.❌(human-friendly nahi)
-        """
-        print(self.data)
-        # fill edges in matrix
-        for i, j in edges:
-            self.data[i][j] = 1
-            self.data[j][i] = 1
+        self.data = [[]for _ in range(n_vertices)]
+        for v1, v2 in edges:
+            self.data[v1].append(v2)
+            self.data[v2].append(v1)
 
-        for row in self.data:
-            print(row)
+    def __repr__(self):
+        result = ""
+        for i, j in enumerate(self. data):
+            result += f" {i}:{j}\n"  # is me pura age ga += ke wajah se
+            # result = f" {i}:{j}\n" # is me sirf ye ae ga  4:[1, 3, 0]  pur nahe .
+            """to har iteration me result overwrite ho jaata —
+               sirf last line bachi rehti.
+               Isliye += use karte hain, taki har line append (add) hoti jaaye."""
+        return result
 
 
-edges = [(0, 1), (0, 2), (1, 2), (2, 3)]
-g = GraphMatrix(4, edges)
+graphs = Graph(5, [(0, 1), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4), (4, 0)])
+
+print(graphs)
+
+
+# class GraphMatrix:
+#     def __init__(self, n_vertices, edges):
+# Intailize a matrix of all zeros
+# self.data = [[0 for _ in range(n_vertices)]for _ in range(n_vertices)]
+# """Loop chal ke 4 rows bana chuka hai ✅
+#    Lekin print karte waqt Python ne sab ek hi line me dikha diya.❌(human-friendly nahi)
+# """
+#         print(self.data)
+#         # fill edges in matrix
+#         for i, j in edges:
+#             self.data[i][j] = 1
+#             self.data[j][i] = 1
+
+#         for row in self.data:
+#             print(row)
+
+
+# edges = [(0, 1), (0, 2), (1, 2), (2, 3)]
+# g = GraphMatrix(4, edges)
+class GraphList:
+    def __init__(self, n_vertices, edges):
+        self.data = [[0] for _ in range(n_vertices)]
+        print(f'fs{self.data}')
+        for u, v in edges:
+            self.data[u].append(v)
+            self.data[v].append(u)
+            print(f'sf{self.data}')
+
+    def bfs(graphs, source):
+        visited = [False] * len(graphs.data)
+        queue = [source]
+        visited[source] = True
+        i = 0
+        while i < len(queue):
+            current = queue[i]
+            for v in graphs.data[current]:
+                distance = 0
+                if not visited[v]:
+                    queue.append(v)
+                    visited[v] = True
+
+            i += 1
+        return queue
+
+
+'''🔹 Tera sawal ka short answer:
+
+matlab ke graphs = 4 or us 4 ke jo connection bane ke vo data ho ke?
+
+✅ Bilkul haan (yes)
+Lekin technically graphs ek object hai jisme:
+
+4 vertices hain
+
+aur unke connections graphs.data ke andar store hain'''
+'''“Source node” = “Starting point” = “The first node from which BFS begins.”'''
+edges = [(0, 1), (0, 2), (1, 3), (2, 4)]
+graphs = GraphList(5, edges)
+
+print(graphs.bfs(0))
