6e9387b7710c87aa233b2f8cf33c3c395cb013ee
arizsalman <muhammadarizsalman@gmail.com>
2025-10-13 20:36:33 +0500

    error 👎

--- Practicce_in DSA[/DSA_Practie.py
+++ Practicce_in DSA[/DSA_Practie.py
@@ -27,3 +27,26 @@ def isAnagram(s, t):
 print(isAnagram(s="anagm", t="nam"))  # Yaha first equation called hora rahe he
 print(isAnagram(s="anagram", t="nagaram"))  # yaha  second
 print(isAnagram(s="rat", t="car"))  # yaha third equation called hu rahe he
+
+
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+# Practice 206
+
+def func(head):
+    stack = []
+    current = head
+    while current:
+        stack.append(current)
+        current = current.next
+
+    left, right = 0, len(head)-1
+    while left < right:
+        if head in left:
+            left += 1
+        right -= 1
+    return stack
+
+
+print(func([1, 2, 3, 4, 5]))
